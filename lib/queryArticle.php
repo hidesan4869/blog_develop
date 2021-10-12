80d9f8c59270e366b815da3e3d828a8cd00783d2
hidesan4869 <t1123.gogo@gmail.com>
2021-10-13 08:24:33 +0900

    画像処理関数に切り分け

--- lib/queryArticle.php
+++ lib/queryArticle.php
@@ -2,11 +2,41 @@
     class QueryArticle extends connect {
         private $article;
 
+        const THUMBS_WIDTH = 200;
+
         public function __construct() {
             parent::__construct();
         }
         public function setArticle(Article $article) {
             $this->article = $article;
+        }
+
+        private function saveFile($old_name) {
+            if ($file = $this->article->getFile()) {
+                $old_name = $file['tmp_name'];
+                $new_name = date('YmdHis').mt_rand();
+                $is_upload = false;
+                $type = exif_imagetype($old_name);
+    
+                switch ($type){
+                  case IMAGETYPE_JPEG:
+                    $new_name .= '.jpg';
+                    $is_upload = true;
+                    break;
+                  case IMAGETYPE_GIF:
+                    $new_name .= '.gif';
+                    $is_upload = true;
+                    break;
+                  case IMAGETYPE_PNG:
+                    $new_name .= '.png';
+                    $is_upload = true;
+                    break;
+                }   
+                if ($is_upload && move_uploaded_file($old_name, __DIR__.'/../album/'.$new_name)){
+                  $this->article->setFilename($new_name);
+                  $filename = $this->article->getFilename();
+                }   
+              }
         }
 
         /*
@@ -29,30 +59,10 @@
      
             } else {
                 if ($file = $this->article->getFile()) {
-                  $old_name = $file['tmp_name'];
-                  $new_name = date('YmdHis').mt_rand();
-                  $is_upload = false;
-                  $type = exif_imagetype($old_name);
-      
-                  switch ($type){
-                    case IMAGETYPE_JPEG:
-                      $new_name .= '.jpg';
-                      $is_upload = true;
-                      break;
-                    case IMAGETYPE_GIF:
-                      $new_name .= '.gif';
-                      $is_upload = true;
-                      break;
-                    case IMAGETYPE_PNG:
-                      $new_name .= '.png';
-                      $is_upload = true;
-                      break;
-                  }   
-                  if ($is_upload && move_uploaded_file($old_name, __DIR__.'/../album/'.$new_name)){
-                    $this->article->setFilename($new_name);
+                    $this->article->setFilename($this->saveFile($file['tmp_name']));
                     $filename = $this->article->getFilename();
-                  }   
                 }
+                
                 $stmt = $this->dbh->prepare("INSERT INTO articles (title, body, filename, created_at, updated_at) VALUES  (:title, :body, :filename, NOW(), NOW())");
                 $stmt->bindParam(':title', $title, PDO::PARAM_STR);
                 $stmt->bindParam(':body', $body, PDO::PARAM_STR);
